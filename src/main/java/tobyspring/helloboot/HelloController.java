604263dcd35a6165c8fa3970321c39c559459f5f
sangho0n <tkdgns0917@gmail.com>
2023-01-30 23:04:42 +0900

    서블릿 생성 후 서블릿 컨테이너에서 맵핑이 일어나도록 설정

---
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -0,0 +1,15 @@
+package tobyspring.helloboot;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String helo(String name){
+        return "Hello " + name;
+    }
+
+}
